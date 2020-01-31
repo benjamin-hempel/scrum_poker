2234851ef5bdc601abee11da542fa0480774a5a9
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-01-31 13:55:25 +0100

    Fix client notification

--- Hubs/RoomHub.cs
+++ Hubs/RoomHub.cs
@@ -21,20 +21,24 @@ namespace scrum_poker.Hubs
 
         public string JoinRoom(string roomId, string username)
         {
+            // Check if room exists
             bool roomExists = Rooms.Exists(x => x.Id == roomId);
             if (roomExists == false) return null;
 
             Room room = Rooms.Find(x => x.Id == roomId);
             string userId = room.AddUser(username);
 
+            // Add connection associated with user
             string connectionId = Context.ConnectionId;
             Connections.Add(connectionId, userId);
 
+            // Notify other clients
             List<string> roomUsers = new List<string>();
             foreach(var user in room.Users)
             {
                 string roomUserId = user.Id;
-                roomUsers.Add(roomUserId);
+                string roomUserConnectionId = Connections.First(x => x.Value == roomUserId).Key;
+                if (roomUserId != userId) roomUsers.Add(roomUserConnectionId);
             }
             Clients.Clients(roomUsers).SendAsync("UserJoined", userId, username);
 
@@ -43,19 +47,24 @@ namespace scrum_poker.Hubs
 
         public void LeaveRoom(string roomId, string userId)
         {
+            // Remove user from room
             Room room = Rooms.Find(x => x.Id == roomId);
             room.RemoveUser(userId);
 
+            // Remove connection associated with user
             string connectionId = Context.ConnectionId;
             Connections.Remove(connectionId);
 
+            // Remove room if it's empty
             if (room.Users.Count == 0) Rooms.Remove(room);
 
+            // Notify other clients
             List<string> roomUsers = new List<string>();
-            foreach(var user in room.Users)
+            foreach (var user in room.Users)
             {
                 string roomUserId = user.Id;
-                roomUsers.Add(roomUserId);
+                string roomUserConnectionId = Connections.First(x => x.Value == roomUserId).Key;
+                if (roomUserId != userId) roomUsers.Add(roomUserConnectionId);
             }
             Clients.Clients(roomUsers).SendAsync("UserLeft", userId);
         }
