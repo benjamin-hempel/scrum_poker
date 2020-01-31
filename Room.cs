ae89f1c93cfe47fbe0093d6ebcf42617d9b7d698
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-01-31 10:41:00 +0100

    Add Room class

---
+++ Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scrum_poker
+{
+    public class Room
+    {
+        public string Id { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
