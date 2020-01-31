e02cabb02e7f6d02e07d12a3e1935a3c840cad72
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-01-31 12:31:38 +0100

    Add SignalR hub

---
+++ Hubs/RoomHub.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace scrum_poker.Hubs
+{
+    public class RoomHub : Hub
+    {
+        private static List<Room> Rooms = new List<Room>();
+        private static Dictionary<string, string> Connections = new Dictionary<string, string>();
+
+        public string CreateRoom()
+        {
+            Room newRoom = new Room();
+            Rooms.Add(newRoom);
+
+            return newRoom.Id;
+        }
+
+        public string JoinRoom(string roomId, string username)
+        {
+            bool roomExists = Rooms.Exists(x => x.Id == roomId);
+            if (roomExists == false) return null;
+
+            Room room = Rooms.Find(x => x.Id == roomId);
+            string userId = room.AddUser(username);
+
+            string connectionId = Clients.Caller.ToString();
+            Connections.Add(connectionId, userId);
+
+            return userId;
+        }
+
+        public void LeaveRoom(string roomId, string userId)
+        {
+            Room room = Rooms.Find(x => x.Id == roomId);
+            room.RemoveUser(userId);
+
+            string connectionId = Clients.Caller.ToString();
+            Connections.Remove(connectionId);
+
+            if (room.Users.Count == 0) Rooms.Remove(room);
+        }
+    }
+}
