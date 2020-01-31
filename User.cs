ef5bece03a81975410782bfc8ae9cfc19edda794
Benjamin Hempel <benjamin.hempel@htw-dresden.de>
2020-01-31 10:40:51 +0100

    Add User class

---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scrum_poker
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string SelectedCard { get; set; }
+    }
+}
