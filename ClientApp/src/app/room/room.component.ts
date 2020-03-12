c3c81ae0b92598289324835a3c1599558cbdc213
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-03-12 15:27:58 +0100

    Display card set in room component

--- ClientApp/src/app/room/room.component.ts
+++ ClientApp/src/app/room/room.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { cards } from '../cards';
+
 @Component({
   selector: 'app-room-component',
   templateUrl: './room.component.html'
 })
 export class RoomComponent {
   public currentCount = 0;
+  cards = cards;
 
   public incrementCounter() {
     this.currentCount++;
