7e8cc0aad2903f475262261efd576986aed59285
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-05-15 11:39:50 +0200

    Add test project

---
+++ scrum_poker.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace scrum_poker.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
