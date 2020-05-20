0dad622f693ba1c782c612aad4ace1a25a59a44b
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-05-20 16:27:45 +0200

    Add first RoomHub.JoinRoom scenarios

--- scrum_poker.Tests/Bindings/RoomHubSteps.cs
+++ scrum_poker.Tests/Bindings/RoomHubSteps.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using TechTalk.SpecFlow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+using Moq;
+using Microsoft.AspNetCore.SignalR;
 
 namespace scrum_poker.Tests.Bindings
 {
@@ -11,7 +14,13 @@ namespace scrum_poker.Tests.Bindings
     {
         private Hubs.RoomHub RoomHub = new Hubs.RoomHub();
 
+        private Dictionary<string, string> JoinReturnValues = new Dictionary<string, string>();
+
+        private Mock<HubCallerContext> mockHubCallerContext;
+        private Mock<IHubCallerClients> mockCallerClients;
+
         [When(@"I add a new room to the hub with the card deck ""(.*)""")]
+        [Given(@"I have added a new room to the hub with the card deck ""(.*)""")]
         public void WhenIAddANewRoomToTheHubWithTheCardDeck(String cardDeck)
         {
             string roomId = RoomHub.CreateRoom(cardDeck, false);
@@ -28,11 +37,67 @@ namespace scrum_poker.Tests.Bindings
             Assert.AreEqual(expectedRoomCount, actualRoomCount, $"The hub should contain {expectedRoomCount} rooms");
         }
 
+        [When(@"I let a user with username ""(.*)"" join room ""(.*)""")]
+        public void WhenILetAUserWithUsernameJoinRoom(string username, int roomIndex)
+        {
+            UpdateMockConnectionId();
+            
+            Models.Room room = Hubs.RoomHub.Rooms[roomIndex - 1];
+            string roomId = room.Id;
+            Assert.IsNotNull(roomId, $"Room {roomIndex} does not exist.");
+
+            string result = RoomHub.JoinRoom(roomId, username);
+            JoinReturnValues.Add(username, result);
+        }
+
+        [Then(@"room ""(.*)"" should contain ""(.*)"" users")]
+        public void ThenRoomShouldContainUsers(int roomIndex, int expectedUserCount)
+        {
+            Models.Room room = Hubs.RoomHub.Rooms[roomIndex - 1];
+            List<Models.User> users = room.GetUsers();
+
+            Assert.AreEqual(expectedUserCount, users.Count, $"Room {roomIndex} should contain {expectedUserCount} users.");
+        }
+
+        [Then(@"the return value for user ""(.*)"" should be valid JSON")]
+        public void ThenTheReturnValueForUserInRoomShouldBeValidJSON(string username)
+        {
+            string json = JoinReturnValues[username];
+
+            try
+            {
+                JsonSerializer.Deserialize<object>(json);
+            }
+            catch(JsonException)
+            {
+                Assert.IsTrue(false, $"The return value {json} is not valid JSON.");
+            }
+        }
+
+        public void UpdateMockConnectionId()
+        {
+            mockHubCallerContext.Setup(c => c.ConnectionId).Returns(Guid.NewGuid().ToString());
+            RoomHub.Context = mockHubCallerContext.Object;
+        }
+
+        #region BeforeAfterMethods
+
+        [BeforeScenario]
+        public void CreateMocks()
+        {
+            mockHubCallerContext = new Mock<HubCallerContext>();
+            mockCallerClients = new Mock<IHubCallerClients>();
+
+            RoomHub.Clients = mockCallerClients.Object;
+        }
+
         // RoomHub.Rooms is static and thus shared between all instances, so it needs to be cleared after every scenario
         [AfterScenario]
         public void ClearRoomHub()
         {
             Hubs.RoomHub.Rooms.RemoveRange(0, Hubs.RoomHub.Rooms.Count);
         }
+
+        #endregion
     }
 }
---
+++ scrum_poker.Tests/Specs/RoomHub/JoinRoom.feature
@@ -0,0 +1,32 @@
+﻿Feature: JoinRoom
+	In order to enable users to play together
+	as a developer
+	I want to enable users to join a room.
+
+Scenario: Single user joins single room
+	Given I have added a new room to the hub with the card deck "1,2,3,4,5,6"
+	When I let a user with username "Jim Hopper" join room "1"
+	Then room "1" should contain "1" users
+	And the return value for user "Jim Hopper" should be valid JSON
+
+Scenario: Multiple users join single room
+	Given I have added a new room to the hub with the card deck "1,2,3,4,5,6"
+	When I let a user with username "Jim Hopper" join room "1"
+	And I let a user with username "Jane Hopper" join room "1"
+	And I let a user with username "Joyce Byers" join room "1"
+	Then room "1" should contain "3" users
+	And the return value for user "Jim Hopper" should be valid JSON
+	And the return value for user "Jane Hopper" should be valid JSON
+	And the return value for user "Joyce Byers" should be valid JSON
+
+Scenario: Multiple users join multiple rooms
+	Given I have added a new room to the hub with the card deck "1,2,3,4,5,6"
+	And I have added a new room to the hub with the card deck "1,2,3,4,5,6"
+	When I let a user with username "Jim Hopper" join room "1"
+	And I let a user with username "Jane Hopper" join room "1"
+	And I let a user with username "Joyce Byers" join room "2"
+	Then room "1" should contain "2" users
+	And room "2" should contain "1" users
+	And the return value for user "Jim Hopper" should be valid JSON
+	And the return value for user "Jane Hopper" should be valid JSON
+	And the return value for user "Joyce Byers" should be valid JSON
