fec5e8382a47a9431024d1436ef7e6cfb2550c2e
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-06-19 11:45:46 +0200

    Add mock classes equivalent to the actual backend's classes

--- scrum_poker/ClientApp/src/app/models/mocks/mock-room.js
+++ scrum_poker/ClientApp/src/app/models/mocks/mock-room.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var uuid_1 = require("uuid");
+var mock_user_1 = require("./mock-user");
+/* This is the TypeScript equivalent of the backend's Room class, sans connection management */
 var MockRoom = /** @class */ (function () {
     function MockRoom(cardDeck, allUsersAreAdmins) {
         if (allUsersAreAdmins === void 0) { allUsersAreAdmins = false; }
@@ -10,6 +12,28 @@ var MockRoom = /** @class */ (function () {
         this.allUsersAreAdmins = allUsersAreAdmins;
         this.cardDeck = cardDeck;
     }
+    MockRoom.prototype.addUser = function (username, connectionId) {
+        var newUser;
+        if (this.users.length == 0 || this.allUsersAreAdmins == true)
+            newUser = new mock_user_1.MockUser(username, true);
+        else
+            newUser = new mock_user_1.MockUser(username);
+        this.users.push(newUser);
+        return newUser;
+    };
+    MockRoom.prototype.removeUser = function (userId) {
+        var userToRemove = this.users.findIndex(function (x) { return x.id == userId; });
+        this.users.splice(userToRemove, 1);
+    };
+    MockRoom.prototype.getUser = function (userId) {
+        return this.users.find(function (x) { return x.id == userId; });
+    };
+    MockRoom.prototype.getActiveUsers = function () {
+        return this.users.filter(function (x) { return !x.missingInAction; });
+    };
+    MockRoom.prototype.getAllUSers = function () {
+        return this.users;
+    };
     return MockRoom;
 }());
 exports.MockRoom = MockRoom;
--- scrum_poker/ClientApp/src/app/models/mocks/mock-room.ts
+++ scrum_poker/ClientApp/src/app/models/mocks/mock-room.ts
@@ -1,10 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import { User } from '../user';
+import { MockUser } from './mock-user';
 
+/* This is the TypeScript equivalent of the backend's Room class, sans connection management */
 export class MockRoom {
   public id: string;
-  public users: Array<User>;
+  public users: Array<MockUser>;
   public cardsRevealed: boolean;
   public playedCards: number;
   public allUsersAreAdmins: boolean;
@@ -12,9 +13,38 @@ export class MockRoom {
 
   constructor(cardDeck: string, allUsersAreAdmins: boolean = false) {
     this.id = uuidv4();
-    this.users = new Array<User>();
+    this.users = new Array<MockUser>();
     this.cardsRevealed = false;
     this.allUsersAreAdmins = allUsersAreAdmins;
     this.cardDeck = cardDeck;
+  }
+
+  public addUser(username: string, connectionId: string): MockUser {
+    var newUser: MockUser;
+
+    if (this.users.length == 0 || this.allUsersAreAdmins == true)
+      newUser = new MockUser(username, true);
+    else
+      newUser = new MockUser(username);
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+
+  public removeUser(userId: string): void {
+    var userToRemove: number = this.users.findIndex(x => x.id == userId);
+    this.users.splice(userToRemove, 1);
+  }
+
+  public getUser(userId: string): MockUser {
+    return this.users.find(x => x.id == userId);
+  }
+
+  public getActiveUsers(): Array<MockUser> {
+    return this.users.filter(x => !x.missingInAction);
+  }
+  public getAllUSers(): Array<MockUser> {
+    return this.users;
   }
 }
