2be6a057b0bdc5944492b8e268200166ad4a452e
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-07-16 11:05:13 +0200

    Add RemoveUserFromRoom NUnit tests

--- scrum_poker.Tests.NUnit/RoomTests/RemoveUserFromRoom.cs
+++ scrum_poker.Tests.NUnit/RoomTests/RemoveUserFromRoom.cs
@@ -13,7 +13,13 @@ namespace scrum_poker.Tests.NUnit.RoomTests
         [Test]
         public void RemoveSingleUserFromRoom()
         {
-            Assert.Pass();
+            var room = new Models.Room("1,2,3,4,5,6");
+            var jimHopper = room.AddUser("Jim Hopper", "1");
+            room.AddUser("Jane Hopper", "2");
+            room.RemoveUser(jimHopper.Id);
+
+            int actualUserCount = room.GetAllUsers().Count;
+            Assert.AreEqual(1, actualUserCount, "The room should contain 1 user");
         }
     }
 }
