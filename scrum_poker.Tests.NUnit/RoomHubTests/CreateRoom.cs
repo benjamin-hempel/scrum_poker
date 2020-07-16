d03a986964d5a7776d97099f3aa4ad78ca5d72df
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-07-16 11:05:30 +0200

    Add RoomHub/CreateRoom NUnit tests

--- scrum_poker.Tests.NUnit/RoomHubTests/CreateRoom.cs
+++ scrum_poker.Tests.NUnit/RoomHubTests/CreateRoom.cs
@@ -4,6 +4,8 @@ namespace scrum_poker.Tests.NUnit.RoomHubTests
 {
     class CreateRoom
     {
+        private Hubs.RoomHub roomHub = new Hubs.RoomHub();
+
         [SetUp]
         public void SetUp()
         {
@@ -13,13 +15,27 @@ namespace scrum_poker.Tests.NUnit.RoomHubTests
         [Test]
         public void AddSingleRoom()
         {
-            Assert.Pass();
+            roomHub.CreateRoom("1,2,3,4,5,6", false);
+
+            int actualRoomCount = Hubs.RoomHub.Rooms.Count;
+            Assert.AreEqual(1, actualRoomCount, "The hub should contain 1 room");
         }
 
         [Test]
         public void AddMultipleRooms()
         {
-            Assert.Pass();
+            roomHub.CreateRoom("1,2,3,4,5,6", false);
+            roomHub.CreateRoom("1,2,3,4,5,6", false);
+            roomHub.CreateRoom("1,2,3,4,5,6", false);
+
+            int actualRoomCount = Hubs.RoomHub.Rooms.Count;
+            Assert.AreEqual(3, actualRoomCount, "The hub should contain 3 rooms");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Hubs.RoomHub.Rooms.RemoveRange(0, Hubs.RoomHub.Rooms.Count);
         }
     }
 }
