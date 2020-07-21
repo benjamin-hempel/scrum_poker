362d7b2473dfa93c74639d16567aa84ea15dc761
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-07-21 12:41:20 +0200

    First working frontend test

--- scrum_poker/ClientApp/src/app/services/room.service.spec.ts
+++ scrum_poker/ClientApp/src/app/services/room.service.spec.ts
@@ -3,24 +3,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { RoomService } from '../services/room.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Room } from '../models/room';
 import { MockBackendService } from './backend/mock-backend.service';
 
 describe('RoomService', () => {
-  var roomService: RoomService;
-  var room: Room;
-
-  beforeEach(async function() {
+  beforeEach(function() {
     TestBed.configureTestingModule({
-      providers: [RoomService, Room],
+      providers: [RoomService, MockBackendService],
       imports: [RouterTestingModule]
     });
-
-    roomService = TestBed.get(RoomService);
   });
 
-  it('should be defined', async function () {
+  it('should be defined', function () {
+    var roomService = TestBed.get(RoomService);
+
     expect(roomService).toBeInstanceOf(RoomService);
-    expect(roomService.backend).toBeInstanceOf(MockBackendService)
+    expect(roomService.backend).toBeInstanceOf(MockBackendService);
   });
 });
