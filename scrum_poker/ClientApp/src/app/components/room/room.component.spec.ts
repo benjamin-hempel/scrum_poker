43f810d41c520dd8885a9e36fed9d63f5db7714f
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-08-03 13:20:39 +0200

    Revert "Add more IDs to simplify testing"
    This reverts commit 218efa1b7e207838dccef4cf6e2fbcfd983d4f37.

--- scrum_poker/ClientApp/src/app/components/room/room.component.spec.ts
+++ scrum_poker/ClientApp/src/app/components/room/room.component.spec.ts
@@ -1,7 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
-import { ActivatedRoute } from "@angular/router";
-import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateTestingModule } from 'ngx-translate-testing';
@@ -14,25 +12,20 @@ import { ErrorPageComponent } from '../error-page/error-page.component';
 import { RoomService } from '../../services/room.service';
 import { Room } from '../../models/room';
 
-import validator from 'validator';
 import { routes } from '../../routes';
-
 
 
 describe('RoomComponent', () => {
   let component: RoomComponent;
   let fixture: ComponentFixture<RoomComponent>;
-  let route: ActivatedRouteStub;
-
   let roomService: RoomService;
   let room: Room;
+  
 
-  beforeEach(async function() {
-    route = new ActivatedRouteStub();
-
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [RoomComponent, HomeComponent, ErrorPageComponent],
-      providers: [RoomService, { provide: ActivatedRoute, useValue: route}],
+      providers: [RoomService],
       imports: [
         RouterTestingModule.withRoutes(routes),
         FontAwesomeModule,
@@ -42,52 +35,18 @@ describe('RoomComponent', () => {
     })
     .compileComponents();
 
-    roomService = await TestBed.get(RoomService);
-    room = await TestBed.get(Room);
+    roomService = TestBed.get(RoomService);
+    room = TestBed.get(Room);
 
-    // Recreate joining process
-    await roomService.createRoom("1,2,3,4,5,6", false);
-    await roomService.joinRoom("Chuck E. Cheese");
-    route.setParamMap({ 'rid': room.roomId, 'uid': room.you.userId });
-    
+    roomService.createRoom("1,2,3,4,5,6", false);
+    roomService.joinRoom("Chuck E. Cheese");
+
     fixture = TestBed.createComponent(RoomComponent);
-    
     component = fixture.componentInstance;
     fixture.detectChanges();
-    
-  });
-  
+  }));
+
   it('should be defined', () => {
     expect(component).toBeDefined();
   });
-
-  it('should show the correct information after joining', async(() => {
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      const roomId: HTMLElement = fixture.nativeElement.querySelector('div#divRoomID > p');
-      expect(validator.isUUID(roomId.innerHTML)).toBeTrue();
-      const username: HTMLElement = fixture.nativeElement.querySelector('div#divOwnUsername > p');
-      expect(username.innerHTML).toEqual('Chuck E. Cheese');
-      const userCounter: HTMLElement = fixture.nativeElement.querySelector('div#divRoomUsers > p');
-      expect(userCounter.innerHTML).toEqual('1');
-      const cardCounter: HTMLElement = fixture.nativeElement.querySelector('div#divPlayedCards > p');
-      expect(cardCounter.innerHTML).toEqual('0');
-      const joinLink: HTMLElement = fixture.nativeElement.querySelector('div#divShareLink > p > a');
-      expect(joinLink.innerHTML).toEqual(window.location.protocol + "//" + window.location.host + "/" + room.roomId);
-    });
-  }));
-
-  it('should correctly indicate a selected card', async(() => {
-    fixture.whenStable().then(() => {
-      fixture.detectChanges();
-      const playCardButton: HTMLButtonElement = fixture.nativeElement.querySelector('button#playCardButton');
-      playCardButton.click();
-      fixture.detectChanges();
-      const card4: HTMLElement = fixture.nativeElement.querySelector('div#card-4');
-      card4.click();
-      fixture.detectChanges();
-      const userCard: HTMLElement = fixture.nativeElement.querySelector('div#otherUsersContainer > div');
-      expect(userCard.classList.contains('bg-success')).toBeTrue();
-    });
-  }));
 });
